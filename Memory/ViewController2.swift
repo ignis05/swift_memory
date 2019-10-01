068b164d06c5daa3af383aeeae4eed6bdec9bc8d
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-10-01 15:20:59 +0200

    Assigning random images

--- Memory/ViewController2.swift
+++ Memory/ViewController2.swift
@@ -28,6 +28,30 @@ class ViewController2: UIViewController {
         
         let screenSize: CGRect = UIScreen.main.bounds
         
+        var obrazkiAll:Array<String> = []
+            obrazkiAll.append("cabbage")
+            obrazkiAll.append("carrot")
+            obrazkiAll.append("cauli")
+            obrazkiAll.append("ch-cabbage")
+            obrazkiAll.append("corn")
+            obrazkiAll.append("cucumber")
+            obrazkiAll.append("eggplant")
+            obrazkiAll.append("g-onion")
+            obrazkiAll.append("g-pepper")
+            obrazkiAll.append("ginger")
+            obrazkiAll.append("garlic")
+            obrazkiAll.append("j-radish")
+            obrazkiAll.append("l-lettuce")
+            obrazkiAll.append("lettuce")
+            obrazkiAll.append("potato")
+            obrazkiAll.append("red-onion")
+            obrazkiAll.append("snowpea")
+        
+        var obrazki = obrazkiAll.prefix(width * heigth / 2)
+        obrazki = obrazki + obrazki
+        
+        
+        
         var index:Int = 0
         let scrh:Int = Int(screenSize.height) + 70
         let scrw:Int = Int(screenSize.width)
@@ -35,13 +59,16 @@ class ViewController2: UIViewController {
         for h in 0...heigth-1 {
             for w in 0...width-1 {
                 let button : UIButton = UIButton()
-                let img :UIImage = UIImage(named : "img/none.jpg")!
+//                let img :UIImage = UIImage(named : "img/none.jpg")!
+                let rand = Int.random(in: 0 ... obrazki.count - 1 )
+                let img2 :UIImage = UIImage(named : "img/" + obrazki[rand] + ".png")!
+                obrazki.remove(at: rand)
                 let wpos = blockw / width
                 let blockh:Int = heigth * (wpos + 2)
                 let hpos:Int = (scrh  - blockh) / 2
                 button.frame = CGRect(x:4 + w * (wpos + 2), y:hpos + (wpos + 2) * h, width: wpos, height: wpos) // rozmiar i punkt wstwienia
                 button.tag = index //mozna dodac liczbowy tag
-                button.setBackgroundImage(img, for: UIControl.State.normal) // dodanie tła
+                button.setBackgroundImage(img2, for: UIControl.State.normal) // dodanie tła
                 self.view.addSubview(button) //dodanie buttona do widoku
                 index = index + 1
             }
